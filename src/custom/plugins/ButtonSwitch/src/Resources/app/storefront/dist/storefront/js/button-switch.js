a880ad4af0f4649c5d6082aca0ca82a7cc0851c3
Olgunkal <olgunkalyoncuo@live.de>
2023-07-19 22:17:05 +0200

    plugins added + read.me fixed

---
+++ src/custom/plugins/ButtonSwitch/src/Resources/app/storefront/dist/storefront/js/button-switch.js
@@ -0,0 +1,23 @@
+
+let button= document.querySelector('btn-buy');
+
+
+
+$( document ).ready(function() {
+    $( ".btn-buy" ).click(function() {
+      $( ".btn-buy:focus" ).css('background', '#625f5f');
+      
+      const buttonElement = document.getElementsByClassName("btn btn-block btn-buy")
+      for(const button in buttonElement) {
+        //hier muss noch ein funktionierendes if rein!!!
+        if(buttonElement.click){
+        buttonElement[button].innerText = "Adding to the cart"}}
+      
+      
+      setTimeout(function() {
+      $('.btn-buy').css('background', '#008490');
+      for(const button in buttonElement) {
+        buttonElement[button].innerText = "Add to shopping cart"}
+       }, 1000); 
+    });
+});
---
+++ src/custom/plugins/FashionEight/src/FashionEight.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace FashionEight;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class FashionEight extends Plugin
+{
+}
